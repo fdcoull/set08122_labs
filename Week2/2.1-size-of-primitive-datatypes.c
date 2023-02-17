883dad3ef473f1d72f4d3b55deabfa5670204e0b
Finlay Daniel Coull <58175445+fdcoull@users.noreply.github.com>
2023-02-17 11:43:14 +0000

    Update task 2.1

--- Week2/2.1-size-of-primitive-datatypes.c
+++ Week2/2.1-size-of-primitive-datatypes.c
@@ -8,15 +8,19 @@ int main()
     char charVariable;
     size_t size;
 
+    //Check size of integer
     size = sizeof(integerVariable);
     printf("Size of integer variable is %zu bytes\n", size);
 
+    //Check size of float
     size = sizeof(floatVariable);
     printf("Size of float variable is %zu bytes\n", size);
 
+    //Check size of double
     size = sizeof(doubleVariable);
     printf("Size of double variable is %zu bytes\n", size);
 
+    //Check size of char
     size = sizeof(charVariable);
     printf("Size of char variable is %zu bytes\n", size);
 
